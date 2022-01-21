786b4fd107192baac79ed5c3a922bdc902c5d5b1
dmitryevstratov <evstratovdmitry1992@gmail.com>
2022-01-21 19:45:39 +0300

    Начало проекта

---
+++ src/main/java/com/gmail/shepard1992/familybudgetv1/HelloController.java
@@ -0,0 +1,15 @@
+package com.gmail.shepard1992.familybudgetv1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
