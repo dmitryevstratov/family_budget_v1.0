0d3688a3acafe31fbe524e6994b3d94e40ba0997
dmitryevstratov <evstratovdmitry1992@gmail.com>
2022-02-12 12:16:35 +0300

    clear

--- src/main/java/com/gmail/shepard1992/familybudgetv1/api/ShowViewsApi.java
+++
@@ -1,9 +0,0 @@
-package com.gmail.shepard1992.familybudgetv1.api;
-
-public interface ShowViewsApi {
-
-    void showReportView();
-
-    void initRootView();
-
-}
--- src/main/java/com/gmail/shepard1992/familybudgetv1/constants/ViewsPath.java
+++
@@ -1,9 +0,0 @@
-package com.gmail.shepard1992.familybudgetv1.constants;
-
-public interface ViewsPath {
-
-    String START_VIEW = "/views/startView.fxml";
-
-    String REPORT_VIEW = "/views/reportView.fxml";
-
-}
